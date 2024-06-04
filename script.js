9fad99d20828f93663ae3eaf18024565ab934b7f
Izaan Khan <izaankhan.x@gmail.com>
2024-06-04 21:24:57 +0500

    added hero page content and animation

---
+++ script.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', (event) => {
+    const elements = document.querySelectorAll('.half-logo');
+    elements[0].classList.add('img1-loader');  
+    elements[1].classList.add('img2-loader');  
+    
+  });
+  
